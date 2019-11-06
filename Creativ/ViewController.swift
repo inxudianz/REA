bfd2339926b1e35c704124acfef78867c5843c0c
inxudianz <william.inxcorp@gmail.com>
2019-11-06 16:59:17 +0700

    Initial Commit

---
+++ Creativ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Creativ
+//
+//  Created by William Inx on 06/11/19.
+//  Copyright © 2019 William Inx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
