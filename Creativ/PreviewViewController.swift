18abf58ebcc569c132109d28eecb416ccf095af5
Robby Awalul Meviansyah Abdillah <robbyawalul@gmail.com>
2019-11-07 15:57:14 +0700

    Add Storyboard & View Controller

---
+++ Creativ/PreviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PreviewViewController.swift
+//  Creativ
+//
+//  Created by Robby Awalul Meviansyah Abdillah on 07/11/19.
+//  Copyright © 2019 William Inx. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
