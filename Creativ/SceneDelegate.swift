a017d3f0f8973b023746fcb84e4aefa50e7fac2d
Evan Christian <evanchristian98@gmail.com>
2019-11-08 14:08:56 +0700

    connect onboarding to home user defaults for onboarding

--- Creativ/SceneDelegate.swift
+++ Creativ/SceneDelegate.swift
@@ -17,6 +17,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        let defaults = UserDefaults.standard
+        if defaults.object(forKey: "isFirstTime") == nil {
+            defaults.set("No", forKey:"isFirstTime")
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController")
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+        }else{
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
