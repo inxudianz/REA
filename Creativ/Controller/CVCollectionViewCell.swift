ae5bbcea376644a777cd7574ad2a9c10c1b07885
Robby Awalul Meviansyah Abdillah <robbyawalul@gmail.com>
2019-11-08 10:02:53 +0700

    Show PDF Thumbnail

---
+++ Creativ/Controller/CVCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CVCollectionViewCell.swift
+//  Creativ
+//
+//  Created by Robby Awalul Meviansyah Abdillah on 07/11/19.
+//  Copyright © 2019 William Inx. All rights reserved.
+//
+
+import UIKit
+
+class CVCollectionViewCell: UICollectionViewCell {
+    
+}
